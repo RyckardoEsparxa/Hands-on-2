11cb851a13ae5de792c18984712cefa074e78c1d
RyckardoEsparxa <95117423+RyckardoEsparxa@users.noreply.github.com>
2024-10-25 21:39:39 -0600

    Update create.bat
    - Implementar un Batch Script en Windows, llamado create.bat, para:
    
    Crear un archivo de texto plano llamado mytext.txt; agregando a dicho archivo la cadena “Hola Mundo”
    
    Desplegar/Imprimir en la terminal el contenido del archivo mytext.txt.
    
    Crear una subdirectorio (folder) llamado backup
    
    Copiar el archivo mytext.txt al subdirectorio backup
    
    Listar el contenido del subdirectorio backup
    
    Eliminar el archivo mytext.txt del subdir backup
    
    Eliminar el subir backup

--- Create/create.bat
+++ Create/create.bat
@@ -1,1 +1,22 @@
+@echo off
 
+:: Crea el archivo mytext.txt y escribe "Hola Mundo" en él
+echo Hola Mundo > mytext.txt
+
+:: Muestra el contenido de mytext.txt en la consola
+type mytext.txt
+
+:: Crea un subdirectorio llamado backup
+md backup
+
+:: Copia mytext.txt al directorio backup
+copy mytext.txt backup
+
+:: Muestra el contenido del directorio backup
+dir backup
+
+:: Elimina mytext.txt del directorio backup
+del backup\mytext.txt
+
+:: Elimina el directorio backup
+rd backup
